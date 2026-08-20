cd4432d78ff0c18c1efab3cf4990d2493439f719
nihcas-byte <tomersachin54@gmail.com>
2026-08-20 15:20:52 +0530

    another again 6

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "4.80.0"
     }
   }
-  backend "azurerm" {
-    storage_account_name = "storageatfstate"
-    resource_group_name  = "infosys"
-    container_name       = "tfstatecontainer"
-    key                  = "prod.tfstate"
-  }
+  # backend "azurerm" {
+  #   storage_account_name = "storageatfstate"
+  #   resource_group_name  = "infosys"
+  #   container_name       = "tfstatecontainer"
+  #   key                  = "prod.tfstate"
+  # }
 }
 provider "azurerm" {
   features {}
